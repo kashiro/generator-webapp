a8cfbf0b1c1eabb67e7940e4a31cdd1f910fd098
Tomoyuki Kashiro <ksrtmyk@gmail.com>
2015-01-29 16:41:57 +0900

    Update watch.js

--- app/templates/grunt/watch.js
+++ app/templates/grunt/watch.js
@@ -1,6 +1,5 @@
 var appSettings = require('../settings/app.json'),
-    networkSettings = require('../settings/network.json'),
-    scriptsSettings = require('../settings/scripts.json');
+    networkSettings = require('../settings/network.json');
 
 module.exports = {
     // Watches files for changes and runs tasks based on the changed files
@@ -8,18 +7,18 @@ module.exports = {
         spawn: false
     },
     js: {
-        files: scriptsSettings.app,
+        files: [ appSettings.dir.app + '/scripts/{,*/}*.js' ],
         tasks: ['newer:jshint'],
         options: {
             livereload: true
         }
     },<% if (includeJade) { %>
     jade: {
-        files: [ appSettings.dir.app + '/jade/**/*.jade'],
+        files: [ appSettings.dir.app + '/jade/{,*/}*.jade'],
         tasks: ['jade:server']
     },<% } %><% if (includeCompass) { %>
     compass: {
-        files: [ appSettings.dir.app + '/styles/**/*.{scss,sass}'],
+        files: [ appSettings.dir.app + '/styles{,*/}*.{scss,sass}'],
         tasks: ['compass:server', 'autoprefixer']
     },<% } %>
     livereload: {
@@ -27,7 +26,7 @@ module.exports = {
             livereload: networkSettings.port.livereload
         },
         files: [
-            '.tmp/**/*.html',
+            '.tmp/{,*/}*..html',
             '.tmp/styles/{,*/}*.css',
             appSettings.dir.app + '/images/**/*'
         ]
